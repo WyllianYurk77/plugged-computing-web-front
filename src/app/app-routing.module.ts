9373d2b7c2d9a9e154e413930a4955c484dc05c9
Wyllian Yurk <wyllian.mendes@dcx.ufpb.br>
2022-10-09 22:28:04 -0300

    #4: criando a seleção de fases

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LevelSelectionComponent } from './components/level-selection/level-selection.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', component: HomeComponent}
+  {path: '', pathMatch: 'full', component: HomeComponent},
+  {path: 'fases', component: LevelSelectionComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { LevelSelectionComponent } from './components/level-selection/level-selection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LevelSelectionComponent
   ],
   imports: [
     BrowserModule,
