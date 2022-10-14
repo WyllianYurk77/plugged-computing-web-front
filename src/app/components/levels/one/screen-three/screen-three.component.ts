bf9a232a77ed1997266f9a669fb4bc72e5914aec
Wyllian Yurk <wyllian.mendes@dcx.ufpb.br>
2022-10-13 23:52:35 -0300

    #14: criando animações

--- src/app/components/levels/one/screen-three/screen-three.component.ts
+++ src/app/components/levels/one/screen-three/screen-three.component.ts
@@ -1,22 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-screen-three',
   templateUrl: './screen-three.component.html',
-  styleUrls: ['./screen-three.component.css']
+  styleUrls: ['./screen-three.component.css'],
+  animations: [
+    trigger('flipState', [
+      state(
+        'active',
+        style({
+          transform: 'rotateY(179deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in')),
+    ]),
+  ]
 })
 export class ScreenThreeComponent implements OnInit {
 
-  question: string = "O que você percebeu sobre o número de pontos nos cartões?"
+  flip1: string = 'inactive';
+  flip2: string = 'active';
+  flip4: string = 'active';
+  flip8: string = 'inactive';
+  flip16: string = 'active';
+
+  byn1: number = 1;
+  byn2: number = 0;
+  byn4: number = 0;
+  byn8: number = 1;
+  byn16: number = 0;
+
+  question: string = "O que você percebeu sobre o número de pontos nos cartões?";
 
   answers: string[] = ["Possuem metade do valor anterior", "São valores aleatórios", "São a soma do próximo com o anterior", "Estão em ordem crescente"];
 
   constructor(private router: Router) {
-    //this.answers = ["Possuem metade do valor anterior", "São valores aleatórios", "São a soma do próximo com o anterior", "Estão em ordem crescente"];
+
   }
 
   ngOnInit(): void {
+    this.answers.sort(() => Math.random() - 0.5);
+  }
+
+  toggleFlip(card:number): void {
+    if(card == 1) {
+      this.flip1 = (this.flip1 == 'inactive') ? 'active' : 'inactive';
+    } else if(card == 2) {
+      this.flip2 = (this.flip2 == 'inactive') ? 'active' : 'inactive';
+    } else if(card == 4) {
+      this.flip4 = (this.flip4 == 'inactive') ? 'active' : 'inactive';
+    } else if(card == 8) {
+      this.flip8 = (this.flip8 == 'inactive') ? 'active' : 'inactive';
+    } else if(card == 16) {
+      this.flip16 = (this.flip16 == 'inactive') ? 'active' : 'inactive';
+    }
+    this.toggleBynaries();
   }
 
   changeAnswers(value: string) {
@@ -30,6 +77,39 @@ export class ScreenThreeComponent implements OnInit {
     }
     if(value === "10001") {
       this.router.navigate(['fase-1-4']);
+    }
+    this.answers.sort(() => Math.random() - 0.5);
+  }
+
+  toggleBynaries():void {
+    if(this.flip1 === 'active') {
+      setTimeout(()=> {this.byn1 = 0;},400);
+    } else {
+      setTimeout(()=> {this.byn1 = 1;},400);
+    }
+
+    if(this.flip2 === 'active') {
+      setTimeout(()=> {this.byn2 = 0;},400);
+    } else {
+      setTimeout(()=> {this.byn2 = 1;},400);
+    }
+
+    if(this.flip4 === 'active') {
+      setTimeout(()=> {this.byn4 = 0;},400);
+    } else {
+      setTimeout(()=> {this.byn4 = 1;},400);
+    }
+
+    if(this.flip8 === 'active') {
+      setTimeout(()=> {this.byn8 = 0;},400);
+    } else {
+      setTimeout(()=> {this.byn8 = 1;},400);
+    }
+
+    if(this.flip16 === 'active') {
+      setTimeout(()=> {this.byn16 = 0;},400);
+    } else {
+      setTimeout(()=> {this.byn16 = 1;},400);
     }
   }
 
